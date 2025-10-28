1e8e6db8e362856f7e0929cfd59d5d492f75b2d8
DavidSosa12 <dpuente3@ucol.mx>
2025-10-27 18:02:33 -0600

    text labels and apple sounds

--- scripts/manzana.gd
+++ scripts/manzana.gd
@@ -1,8 +1,16 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
-
+@onready var sonido_manzana: AudioStreamPlayer2D = $SonidoManzana
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.incrementa_un_punto()
+	sonido_manzana.play()
+	animated_sprite.visible = false
+	collision_shape.call_deferred("set", "disabled", true)
+
+
+func _on_sonido_manzana_finished() -> void:
 	queue_free()
