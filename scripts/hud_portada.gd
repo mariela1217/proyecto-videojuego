90ce0faff8a4f0f074a93bfafff2dd63b94a069f
mariela1217 <mt2590987@gmail.com>
2025-11-10 17:48:01 -0600

    Se agregaron los animales a la portada y sonido al botón comenzar

--- scripts/hud_portada.gd
+++ scripts/hud_portada.gd
@@ -1,8 +1,16 @@
 extends CanvasLayer
 
+@export var escena_a_cargar: PackedScene
+@export var audio: AudioStream
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_boton_comenzar_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/cutscene_intro.tscn")
+	if escena_a_cargar:
+		AudioPlayer.play(audio)
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		get_tree().change_scene_to_packed(escena_a_cargar)
 
 
 func _on_boton_salir_pressed() -> void:
