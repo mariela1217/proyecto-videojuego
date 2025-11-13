cfec892df6e1b3b00db89e14022b7e9fdac0cbef
mariela1217 <mt2590987@gmail.com>
2025-11-12 18:35:05 -0600

    se cambió el fondo, se agregó sonido y animación al momento de morir

--- scripts/zona_de_muerte.gd
+++ scripts/zona_de_muerte.gd
@@ -1,10 +1,17 @@
 extends Area2D
 @onready var timer: Timer = $Timer
+@onready var sonido_muerte: AudioStreamPlayer2D = $SonidoMuerte
+
 
 func _on_body_entered(body: Node2D) -> void:
+	sonido_muerte.play()
 	print("has perdido!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	
 	get_tree().reload_current_scene()
