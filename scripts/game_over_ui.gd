e8d47962a971ade97bc375043e316eca918c30f2
mariela1217 <mt2590987@gmail.com>
2025-11-25 22:51:14 -0600

    se arregló el error de que los enemigos aparecieran atrás del layer, y también se arregló el problema que cuando moría, mostraba el menú, pero no mostraba el mouse, ya quedó arreglado

--- scripts/game_over_ui.gd
+++ scripts/game_over_ui.gd
@@ -6,11 +6,13 @@ extends CanvasLayer
 func _ready() -> void:
 	retry_button.pressed.connect(_on_retry_pressed)
 	menu_button.pressed.connect(_on_menu_pressed)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_retry_pressed() -> void:
 	print("Retry pressed")
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _on_menu_pressed() -> void:
 	print("Menu pressed")
